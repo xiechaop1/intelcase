ccedb8ea8ed2db057d35b0006fbad16474387dab
xiechao <xiechao2@tal.com>
2025-06-02 21:19:54 +0800

    fix bug

--- frontend/actions/flow/VisitApi.php
+++ frontend/actions/flow/VisitApi.php
@@ -54,9 +54,9 @@ class VisitApi extends ApiAction
                 ->where([
                     'id' => $this->_reportId,
                 ])
-                ->andFilterWhere([
-                    'between', 'visit_time', strtotime(date('Y-m-d 00:00:00')), strtotime(date('Y-m-d 23:59:59'))
-                ])
+//                ->andFilterWhere([
+//                    'between', 'visit_time', strtotime(date('Y-m-d 00:00:00')), strtotime(date('Y-m-d 23:59:59'))
+//                ])
                 ->orderBy([
                     'id' => SORT_DESC
                 ])
--- frontend/controllers/ReportController.php
+++ frontend/controllers/ReportController.php
@@ -9,11 +9,40 @@
 namespace frontend\controllers;
 
 
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 class ReportController extends Controller
 {
     public $layout = '@frontend/views/layouts/main_n.php';
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => [
+                            'add', 'get_by_id', 'update'
+                        ],
+                        'allow' => true,
+                        'roles' => ['?']
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'login' => ['POST'],
+                    'verification-code' => ['POST', 'GET'],
+                    'register' => ['POST'],
+                ],
+            ],
+        ]);
+    }
 
     public function actions()
     {
