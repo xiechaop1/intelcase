aace6fda742fec37bb8525296c3437e87e10c75b
xiechao <xiechao2@tal.com>
2025-06-08 17:56:36 +0800

    fix bug

--- frontend/actions/flow/SubscribedApi.php
+++ frontend/actions/flow/SubscribedApi.php
@@ -72,7 +72,7 @@ class SubscribedApi extends ApiAction
                         'id' => $this->_reportId,
                     ])
                     ->andFilterWhere([
-                        'between', 'visit_time', strtotime(date('Y-m-d 00:00:00')), strtotime(date('Y-m-d 23:59:59'))
+                        'between', 'visit_time', date('Y-m-d 00:00:00'), date('Y-m-d 23:59:59')
                     ])
                     ->andFilterWhere([
                         'report_status' => Report::REPORT_STATUS_PASS,
