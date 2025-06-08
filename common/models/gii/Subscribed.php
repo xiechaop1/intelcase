3675381950a542b46f4ae07083fa852f38c2dd68
xiechao <xiechao2@tal.com>
2025-06-08 11:39:12 +0800

    fix bug

--- common/models/gii/Subscribed.php
+++ common/models/gii/Subscribed.php
@@ -29,8 +29,8 @@ class Subscribed extends \yii\db\ActiveRecord
             [['project_id', 'report_id', 'visit_id', 'sub_type', 'increase_rate', ], 'integer'],
             [['balance_price', 'sub_total_price', 'supply_total_price', 'daily_amount', 'monthly_amount', 'yearly_amount', 'deposit', 'rent_amount',
                 'pro_rent_amount', 'al_daily_amount', 'al_amount', 'al_other', 'al_total_amount'], 'number'],
-            [['rent_date_begin', 'rent_date_end', 'al_date_begin', 'al_date_end', 'sub_status', ], 'string'],
-            [['status', 'created_at', 'updated_at',], 'integer'],
+            [['rent_date_begin', 'rent_date_end', 'al_date_begin', 'al_date_end',  ], 'string'],
+            [['sub_status','status', 'created_at', 'updated_at',], 'integer'],
         ];
     }
 
