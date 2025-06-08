76f278c795d913f5b6a742b6665f69150d05e703
xiechao <xiechao2@tal.com>
2025-06-08 09:52:37 +0800

    fix bug

--- common/helpers/Payment.php
+++ common/helpers/Payment.php
@@ -16,7 +16,7 @@ class Payment
     public static function checkTotalAmount($payments, $subTotalPrice, $nowPayment = [])
     {
         $payTotal = 0;
-        $payStatus = Subscribed::SUB_PAY_WAIT;
+//        $payStatus = Subscribed::SUB_PAY_WAIT;
         if ($subTotalPrice == 0) return False;
         if (!empty($payments)) {
             foreach ($payments as $pay) {
@@ -36,7 +36,7 @@ class Payment
             }
         }
 
-        if (!empty($payments)) {
+        if (!empty($payments) || !empty($nowPayment)) {
             if ($payTotal > $subTotalPrice) {
                 $payStatus = Subscribed::SUB_PAY_FULLY;
             } else {
