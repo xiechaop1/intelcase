e44e82f28aa11606ba103a4a33525510b46418ab
xiechao <xiechao2@tal.com>
2025-08-12 17:47:24 +0800

    fix bug

--- frontend/actions/flow/DataApi.php
+++ frontend/actions/flow/DataApi.php
@@ -1230,6 +1230,7 @@ class DataApi extends ApiAction
 //                case Staff::STAFF_ROLE_ADMIN_PART:
                 case Staff::STAFF_ROLE_ADMIN_CHILD:
                 case Staff::STAFF_ROLE_FINANCE:
+                    $projectId = [0];
                     break;
                 default:
                     $projectId = [-1];
@@ -1248,6 +1249,9 @@ class DataApi extends ApiAction
         }
         if (empty($projectId)) {
             $projectId = [-1];
+        } elseif (is_array($projectId) && sizeof($projectId) == 1 && $projectId[0] = 0) {
+            $projectId = [];
+
         }
 
         return $projectId;
